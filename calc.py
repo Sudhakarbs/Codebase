45eb73eff47fbf23c6e1d0f3148103e720c0a9d6
Sudhakarbs <sudhakarbs@sify.com>
2013-12-26 04:05:39 +0530

    Calculate area and square

---
+++ calc.py
@@ -0,0 +1,20 @@
+def square_return(num):
+    return num ** 2
+
+def square_print(num):
+    print("The square of the number is ", num ** 2)
+    
+def area(base,height):
+    '''(number,number) -> number
+    Return the area of a triangle with dimensions base and height.
+    >>> area(10,5)
+    25.0
+    >>> area(2.5,3)
+    3.75
+    '''
+    return (base * height) / 2
+
+def announce_location(country):
+    print (country)
+    return country
+
